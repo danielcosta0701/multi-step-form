1c090a86ff10cc4706b68d26dc56d7ab8c6c7c45
danielcosta0701 <contatodanielskt@gmail.com>
2024-07-26 17:40:07 -0300

    fix: fixed masks

--- src/components/forms/inputs/Input/Input.tsx
+++ src/components/forms/inputs/Input/Input.tsx
@@ -1,55 +1,42 @@
-import React from 'react';
-import InputMask from 'react-input-mask';
 import { UseFormRegisterReturn, FieldError } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 import './Input.scss';
 
 interface InputProps {
-  label: string;
-  type?: string;
-  placeholder: string;
-  register: UseFormRegisterReturn;
-  defaultValue?: string | null;
-  error?: FieldError;
-  mask?: string; // Adiciona a propriedade mask
+    label: string;
+    type?: string;
+    placeholder: string;
+    register: UseFormRegisterReturn;
+    defaultValue?: string | null;
+    error?: FieldError;
+    mask?: string;
 }
 
 export const Input = (props: InputProps) => {
-  const { 
-    label, 
-    type = "text", 
-    placeholder,
-    register,
-    defaultValue = '',
-    error,
-    mask, // Desestrutura a nova propriedade mask
-  } = props;
+    const { 
+        label, 
+        type = "text", 
+        placeholder,
+        register,
+        defaultValue = '',
+        error,
+        mask,
+    } = props;
 
-  const InputElement = mask ? (
-    <InputMask
-      mask={mask}
-      defaultValue={defaultValue || ''}
-      {...register}
-      className={error ? 'error' : ''}
-    >
-      {(inputProps) => <input type={type} placeholder={placeholder} {...inputProps} />}
-    </InputMask>
-  ) : (
-    <input
-      type={type}
-      placeholder={placeholder}
-      defaultValue={defaultValue || ''}
-      {...register}
-      className={error ? 'error' : ''}
-    />
-  );
-
-  return (
-    <div className={`input-container ${error ? 'error' : ''}`}>
-      <label>
-        {label}
-      </label>
-      {InputElement}
-      {error && <p className="error-message">{error.message}</p>}
-    </div>
-  );
+    return (
+        <div className={`input-container ${error ? 'error' : ''}`}>
+            <label>
+                {label}
+            </label>
+            <InputMask
+                mask={mask}
+                type={type}
+                placeholder={placeholder}
+                defaultValue={defaultValue || ''}
+                {...register}
+                className={error ? 'error' : ''}
+            />
+            {error && <p className="error-message">{error.message}</p>}
+        </div>
+    );
 }

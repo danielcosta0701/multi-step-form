45a77c63ed3ac970207f220ba38634344ffc4a69
danielcosta0701 <contatodanielskt@gmail.com>
2024-07-28 15:00:21 -0300

    feat: created footer modal

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import './Footer.scss';
+import Swal from 'sweetalert2';
 
 interface FooterProps {
     text: string;
@@ -11,10 +12,29 @@ const Footer = (props: FooterProps) => {
         className
     } = props;
 
+    const handleFooterModal = () => {
+        Swal.fire({
+            title: 'Informações para contato',
+            html: `
+              <div class="footer-modal-content">
+                <div><strong>Telefone:</strong> (11) 97681-4472</div>
+                <div><strong>E-mail:</strong> danielcosta.dev@outlook.com</div>
+                <div><strong>Github:</strong> <a href="https://github.com/danielcosta0701" target="_blank">github.com/danielcosta0701</a></div>
+              </div>
+            `,
+            showConfirmButton: false,
+            showCloseButton: true,
+            customClass: {
+              title: 'footer-modal-title',
+              htmlContainer: 'footer-modal-content'
+            }
+          });
+    }
+
     return (
         <footer className={`footer ${className}`}>
             <div className="footer-content">
-                <a href="http://github.com/danielcosta0701">{text}</a>
+                <a onClick={handleFooterModal}>{text}</a>
             </div>
         </footer>
     );

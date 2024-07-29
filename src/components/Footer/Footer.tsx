ef44cae06256234246f36f8612ff93c25e042b08
danielcosta0701 <contatodanielskt@gmail.com>
2024-07-28 21:59:50 -0300

    feat: last updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,13 +14,17 @@ const Footer = (props: FooterProps) => {
 
     const handleFooterModal = () => {
         Swal.fire({
-            title: 'Informações para contato',
+            title: 'Sobre',
             html: `
               <div class="footer-modal-content">
                 <div><strong>Telefone:</strong> (11) 97681-4472</div>
-                <div><strong>E-mail:</strong> danielcosta.dev@outlook.com</div>
-                <div><strong>Github:</strong> <a href="https://github.com/danielcosta0701" target="_blank">github.com/danielcosta0701</a></div>
-              </div>
+                <div><strong>E-mail:</strong> danielcosta.dev@outlook.com</div>             
+                <div><strong>Github:</strong> <a href="https://github.com/danielcosta0701/multi-step-form" target="_blank">github.com/danielcosta0701</a></div>
+                <div>
+                    <strong>Tecnologias:</strong>
+                    React.js • TypeScript • SASS • Context API • React-Hook-Form • Sweet Alert 2 • React-Input-Mask • Git
+                </div>   
+            </div>
             `,
             showConfirmButton: false,
             showCloseButton: true,
